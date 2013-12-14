7388579bc06693bf0ba8a1693e110964e89c50f1
Xujia Li <lixujia.cn@gmail.com>
2013-12-15 00:41:07 +0800

    m

--- NetServer.py
+++ NetServer.py
@@ -1,4 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import json
 import socketserver
+from Command import CmdGetFunction
+from ModbusMaster import ModbusMaster
+
+mm = ModbusMaster(port = "/dev/ttyUSB0",timeout = 0.2,baudrate = 9600)
+
+def process_request(data):
+    req = json.loads(data.decode())
+
+    if "Address" not in req or "cmd" not in req:
+        return b"Error"
+
+    slave = req["Address"]
+
+    if "GetFunction" == req["cmd"]:
+        cmd = CmdGetFunction(slave)
+    else:
+        return b"Error"
+
+    rsp = mm.execute_cmd(cmd)
+    if None == rsp:
+        return b"Error"
+
+    return json.dumps(rsp)
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
     """
@@ -11,12 +38,10 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
 
-        
+        rsp = process_request(data)
 
-        socket.sendto(data.upper(), self.client_address)
+        socket.sendto(rsp, self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
